872bc89a4a4bc2a68acb135dac8d82dae2e2710b
Steven <gonghaima@hotmail.com>
2020-09-15 11:12:23 +1000

    month validation & test update

--- src/components/AgeField/validation.js
+++ src/components/AgeField/validation.js
@@ -3,7 +3,11 @@ const validateDay = (e) => {
     const val = Number(e.target.value) || Number.MIN_VALUE;
     return val >= 0 && val <= 31;
 };
-const validateMonth = (e) => e.target.value.length <= 2;
+const validateMonth = (e) => {
+    if (e.target.value.length > 2) return false;
+    const val = Number(e.target.value) || Number.MIN_VALUE;
+    return val > 0 && val <= 12;
+};
 const validateYear = (e) => e.target.value.length <= 4;
 
 export { validateDay, validateMonth, validateYear }
